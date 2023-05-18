5bd95b0fc25ad1970cb54ab03cf02ea3fe742f58
Smallten <smallten@smallten.me>
2023-05-18 09:27:41 +0800

    refactor: change stream of Error in log to Stderr

--- log/log.go
+++ log/log.go
@@ -12,7 +12,7 @@ var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001b[0m", log.LstdFlags|log.Lsh
 var Warning = log.New(os.Stdout, "\u001b[33mWARNING: \u001b[0m", log.LstdFlags|log.Lshortfile)
 
 // Error writes logs in the color red with "ERROR: " as prefix
-var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
+var Error = log.New(os.Stderr, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 
 // Debug writes logs in the color cyan with "DEBUG: " as prefix
 var Debug = log.New(os.Stdout, "\u001b[36mDEBUG: \u001b[0m", log.LstdFlags|log.Lshortfile)
